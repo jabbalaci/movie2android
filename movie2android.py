b05a25f39a9bc6cab40d08d720092187d7a38aed
Jabba Laci <jabba.laci@gmail.com>
2013-02-27 19:08:42 +0100

    TODO added

--- movie2android.py
+++ movie2android.py
@@ -1,14 +1,32 @@
 #!/usr/bin/env python
 
 """
+movie2android
+=============
+
+* Author:  Laszlo Szathmary, 2013 (<jabba.laci@gmail.com>)
+* Website: <https://ubuntuincident.wordpress.com/2013/02/26/resize-movies-for-androidios-with-ffmpeg/>
+* GitHub:  <https://github.com/jabbalaci/movie2android>
+
 Resize a movie for your Android/iOS device with ffmpeg.
 
-Get a static FFmpeg build from http://ffmpeg.gusari.org/static/ .
+Get a static FFmpeg build from <http://ffmpeg.gusari.org/static/>.
 
 This mini project was inspired by the LUD-Media-Converter
-(https://github.com/kunaldeo/LUD-Media-Converter).
+(<https://github.com/kunaldeo/LUD-Media-Converter>).
 
-by Jabba Laci (jabba.laci@gmail.com), 2013
+Usage:
+------
+
+    ./movie2android.py movie.avi
+
+It will resize the movie and produce a `movie.mp4` file.
+If the input was called `movie.mp4`, the output will be
+called `movie-resized.mp4`. You can change the ffmpeg settings
+by adjusting the `config` dictionary in the source.
+
+You can also pass *several* parameters to the script and they
+will be processed one by one in a queue.
 """
 
 import os
