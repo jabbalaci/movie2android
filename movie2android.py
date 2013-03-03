c81e8c96cec6e838f9dac9daf6edc784c56dc603
Jabba Laci <jabba.laci@gmail.com>
2013-03-03 20:48:21 +0100

    if the audio codec fails, conversion is retried with a failsafe method

--- movie2android.py
+++ movie2android.py
@@ -29,9 +29,17 @@ You can also pass *several* parameters to the script and they
 will be processed one by one in a queue.
 """
 
+__author__ = "Laszlo Szathmary (jabba.laci@gmail.com)"
+__version__ = "0.1.1"
+__date__ = "20130303"
+__copyright__ = "Copyright (c) 2013 Laszlo Szathmary"
+__license__ = "GPL"
+
 import os
 import sys
 import termcolor
+import shlex
+from subprocess import Popen, PIPE
 
 
 config = {
@@ -41,12 +49,16 @@ config = {
     'height': '320',
     'threads': '2'
 }
+AUDIO_CODEC_DEFAULT = 'libvo_aacenc'
+AUDIO_CODEC_FAILSAFE = 'copy'    # if the previous fails
 
 command = """{ffmpeg} -i %(input)s -codec:v libx264 -quality good -cpu-used 0
 -b:v {bitrate} -profile:v baseline -level 30 -y -maxrate 2000k
--bufsize 2000k -vf scale={width}:{height} -threads {threads} -codec:a libvo_aacenc
+-bufsize 2000k -vf scale={width}:{height} -threads {threads} -codec:a %(audio_codec)s
 -b:a 128k %(output)s""".replace('\n', ' ').format(**config)
 
+audio_codec_problem = "Warning! There was a problem with the audio codec and the conversion failed. " + \
+    "Retrying another method..."
 
 def resize(fname):
     """
@@ -60,9 +72,21 @@ def resize(fname):
         print termcolor.colored('Warning: the file {0} exists!'.format(output), "red")
         return
     # else
-    cmd = command % {'input': fname, 'output': output}
+    cmd = command % {'input': fname, 'output': output, 'audio_codec': AUDIO_CODEC_DEFAULT}
     print termcolor.colored(cmd, "green")
-    os.system(cmd)
+    process = Popen(shlex.split(cmd), stdout=PIPE)
+    process.communicate()
+    exit_code = process.wait()
+    if exit_code != 0:
+        print termcolor.colored(audio_codec_problem, "red")
+        os.unlink(output)
+        cmd = command % {'input': fname, 'output': output, 'audio_codec': AUDIO_CODEC_FAILSAFE}
+        print termcolor.colored(cmd, "green")
+        process = Popen(shlex.split(cmd), stdout=PIPE)
+        process.communicate()
+        exit_code = process.wait()
+        if exit_code == 0:
+            print termcolor.colored("Success!", "green")
 
 
 def main(movies):
