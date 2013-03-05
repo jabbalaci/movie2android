899dd336c63dd5ffa0d42f2b79af5c55922d5168
Jabba Laci <jabba.laci@gmail.com>
2013-03-05 22:52:21 +0100

    support for static build and own compilation

--- movie2android.py
+++ movie2android.py
@@ -10,7 +10,18 @@ movie2android
 
 Resize a movie for your Android/iOS device with ffmpeg.
 
-Get a static FFmpeg build from <http://ffmpeg.gusari.org/static/>.
+You have two options:
+
+(1) Get a static FFmpeg build from <http://ffmpeg.gusari.org/static/>.
+It includes the audio codecs libvo_aacenc and aac. They are OK but not very good.
+
+(2) If you want better codecs, you must compile your own FFmpeg,
+as described here: <http://ubuntuincident.wordpress.com/2013/03/05/compile-your-own-ffmpeg/>.
+It will include libfdk_aac and libfaac, where libfdk_aac is
+considered to be the best.
+
+In the config part of the script you must select
+which FFmpeg version you have (set the variable VERSION).
 
 This mini project was inspired by the LUD-Media-Converter
 (<https://github.com/kunaldeo/LUD-Media-Converter>).
@@ -34,8 +45,8 @@ Accepted switches:
 """
 
 __author__ = "Laszlo Szathmary (jabba.laci@gmail.com)"
-__version__ = "0.1.1"
-__date__ = "20130303"
+__version__ = "0.1.2"
+__date__ = "20130305"
 __copyright__ = "Copyright (c) 2013 Laszlo Szathmary"
 __license__ = "GPL"
 
@@ -47,6 +58,13 @@ from subprocess import Popen, PIPE
 import re
 #import pprint
 
+STATIC_BUILD = 1
+OWN_COMPILATION = 2
+
+# select which version you have:
+#VERSION = STATIC_BUILD
+VERSION = OWN_COMPILATION
+
 # all values are strings (even numeric values)
 config = {
     'ffmpeg': '/opt/ffmpeg.static/ffmpeg',
@@ -54,9 +72,14 @@ config = {
     'width': '480',
     'height': '320',
     'threads': '2',
-    'audio_codec': 'libvo_aacenc',
-    'audio_codec_failsafe': 'aac -strict experimental',
+    'audio_codec': 'aac -strict experimental',
+    'audio_codec_failsafe': 'libvo_aacenc',     # if the previous fails
 }
+
+if VERSION == OWN_COMPILATION:
+    config['ffmpeg'] = 'ffmpeg'
+    config['audio_codec'] = 'libfdk_aac'
+    config['audio_codec_failsafe'] = 'libfaac'
 
 command = """{ffmpeg} -i \"%(input)s\" -codec:v libx264 -quality good -cpu-used 0
 -b:v {bitrate} -profile:v baseline -level 30 -y -maxrate 2000k
